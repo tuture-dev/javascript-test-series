94383096931adfda215c8d386a833993e8932105
chenzhe <chenzhe.729@bytedance.com>
2020-08-31 02:59:24 +0800

    迭代 TodoList 组件

--- src/TodoList.js
+++ src/TodoList.js
@@ -8,6 +8,7 @@ const apiUrl = 'https://api.tuture.co';
 class ToDoList extends Component {
   state = {
     tasks: [],
+    newTask: '',
   };
 
   componentDidMount() {
@@ -19,13 +20,38 @@ class ToDoList extends Component {
       .catch((error) => console.log(error));
   }
 
+  addATask = () => {
+    const { newTask, tasks } = this.state;
+
+    if (newTask) {
+      return axios
+        .post(`${apiUrl}/tasks`, { task: newTask })
+        .then((taskResponse) => {
+          const newTasksArray = [...tasks];
+          newTasksArray.push(taskResponse.data.task);
+          this.setState({ tasks: newTasksArray, newTask: '' });
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  handleInputChange = (event) => {
+    this.setState({ newTask: event.target.value });
+  };
+
   render() {
+    const { newTask } = this.state;
     return (
-      <ul>
-        {this.state.tasks.map((task) => (
-          <Task key={task.id} id={task.id} name={task.name} />
-        ))}
-      </ul>
+      <div>
+        <h1>ToDoList</h1>
+        <input onChange={this.handleInputChange} value={newTask} />
+        <button onClick={this.addATask}>Add a task</button>
+        <ul>
+          {this.state.tasks.map((task) => (
+            <Task key={task.id} id={task.id} name={task.name} />
+          ))}
+        </ul>
+      </div>
     );
   }
 }
