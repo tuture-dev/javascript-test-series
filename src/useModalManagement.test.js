4bae33fe0f163d1fed560b1ed32e269253c4097b
chenzhe <chenzhe.729@bytedance.com>
2020-09-08 09:58:19 +0800

    开始使用 react-hooks-testing-library

--- src/useModalManagement.test.js
+++ src/useModalManagement.test.js
@@ -1,8 +1,23 @@
 import useModalManagement from './useModalManagement';
-import testHook from './testHook';
+import { renderHook, act } from '@testing-library/react-hooks';
 
 describe('The useModalManagement hook', () => {
   it('should not throw an error', () => {
-    testHook(useModalManagement);
+    renderHook(() => useModalManagement());
+  });
+
+  it('should describe a closed modal by default', () => {
+    const { result } = renderHook(() => useModalManagement());
+    expect(result.current.isModalOpened).toBe(false);
+  });
+
+  describe('when the openModal function is called', () => {
+    it('should describe an opened modal', () => {
+      const { result } = renderHook(() => useModalManagement());
+      act(() => {
+        result.current.openModal();
+      });
+      expect(result.current.isModalOpened).toBe(true);
+    });
   });
 });
