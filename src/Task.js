02bb38c8699768a9ff26c312b92cc147e0be4cd2
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-01 01:04:04 +0800

    Delete src directory

--- src/Task.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Task = (props) => {
-  return <li>{props.name}</li>;
-};
-
-export default Task;
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import modal from './modal';
-
-const rootReducer = combineReducers({ modal });
-
-export default rootReducer;
